48ff93b98643a6fef3968290c861f60912c2614c
yyh-rain-song <40878399+yyh-rain-song@users.noreply.github.com>
2018-07-06 17:38:55 +0800

    Update memory.h

--- memory.h
+++ memory.h
@@ -5,12 +5,14 @@ namespace yyh
 {
 	extern std::map<std::string, int> reg_num;
 	extern int Registers[33];
+	extern int memory_pos;
 	extern char Memory[4 * 1024 * 1024];
 	extern std::map<std::string, int> Labels;
 	void reg_num_init();
 	int at_register(const std::string& s);
 	void insert_memory_label(const std::string& s);
 	int at_label(const std::string& s);
+	void _align(int n);
 	void _ascii(const std::string &s);
 	void _asciiz(const std::string &s);
 	void _byte(const char a);
