a4c4754db9d3ebe39c8c49bec17a02df0e02872b
xuqingyao <xuqingyao@sjtu.edu.cn>
2018-07-11 16:10:56 +0800

    predict AC

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ namespace yyh
 }
 int main(int argc, char *argv[])
 {
-	freopen("aa.out", "w", stdout);
+	//freopen("aa.out", "w", stdout);
 	token::initialize_keyword();
 	reg_num_init();
-	std::ifstream text("1.s");
+	std::ifstream text(argv[1]);
 	read_in(text);
 	run_();
 	text.close();
